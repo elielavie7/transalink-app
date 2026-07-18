c19ac694872a52e1016bd9a40d1f7084b2956b51
elielavie7 <elielavie7@gmail.com>
2026-07-18 14:40:44 +0200

    mise à jour de BASE_URL pour le d^ploiement render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,7 @@
-const BASE_URL = "http://192.168.1.163:5000";
+// En production sur Render :
+const BASE_URL = "https://onrender.com";
+
+// En local chez vous (désactivé pour l'instant) :
+// const BASE_URL = "http://192.168.1.163:5000";
+
 const API_URL = `${BASE_URL}/api`;
